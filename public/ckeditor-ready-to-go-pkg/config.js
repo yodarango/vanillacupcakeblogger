ed5bc9e5541ba8ec50b1b90c89dee91c4b6ded23
Yodarango <danranngel@gmail.com>
2021-10-01 18:56:14 -0400

    removed imagebase62

--- public/ckeditor-ready-to-go-pkg/config.js
+++ public/ckeditor-ready-to-go-pkg/config.js
@@ -3,9 +3,9 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
+CKEDITOR.editorConfig = function (config) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.extraPlugins = "base64image";
+	//config.extraPlugins = "base64image";
 };
